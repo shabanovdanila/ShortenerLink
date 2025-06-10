a71e8e2bbf0ade73a8946a7c73cae358406e9909
shabanovdanila <168763335+shabanovdanila@users.noreply.github.com>
2025-06-10 17:40:54 +0300

    first push

---
+++ ShortenerLink/ShortenerLink/ShortenerLinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShortenerLinkApp.swift
+//  ShortenerLink
+//
+//  Created by Данила Шабанов on 10.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct ShortenerLinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
